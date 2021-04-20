0aa43d296ecd5d5d19fd2ade0ac4a4db179585f0
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-04-20 19:28:33 +0900

    Refactor: 회원가입 화면 구성

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,19 +1,115 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components/native";
-import { Text } from "react-native";
+import { Image, Input, Button } from "../components";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { validateEmail, removeWhitespace } from "../utils/common";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.background};
+  padding: 40px 20px;
+`;
+
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 10px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.errorText};
 `;
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  useEffect(() => {
+    let _errorMessage = "";
+    if (!name) {
+      _errorMessage = "이름을 적어주세요";
+    } else if (!validateEmail(email)) {
+      _errorMessage = "이메일을 확인해주세요";
+    } else if (password.length < 6) {
+      _errorMessage = "비밀번호는 6자리 이상";
+    } else if (password !== passwordConfirm) {
+      _errorMessage = "비밀번호가 일치하지 않습니다.";
+    } else {
+      _errorMessage = "";
+    }
+    setErrorMessage(_errorMessage);
+  }, [name, email, password, passwordConfirm]);
+
+  useEffect(() => {
+    setDisabled(
+      !(name && email && password && passwordConfirm && !errorMessage)
+    );
+  }, [name, email, password, passwordConfirm]);
+
+  const _handleSignupButtonPress = () => {};
+
   return (
-    <Container>
-      <Text style={{ fontSize: 30 }}>Signup Screen</Text>
-    </Container>
+    <KeyboardAwareScrollView extraScrollHeight={20}>
+      <Container>
+        <Image rounded />
+        <Input
+          label="Name"
+          value={name}
+          onChangeText={(text) => setName(text)}
+          onSubmitEditing={() => {
+            setName(name.trim());
+            emailRef.current.focus();
+          }}
+          onBlur={() => setName(name.trim())}
+          placeholder="Name입력"
+          returnKeyType="next"
+        />
+        <Input
+          ref={emailRef}
+          label="Email"
+          value={email}
+          onChangeText={(text) => setEmail(removeWhitespace(text))}
+          onSubmitEditing={() => passwordRef.current.focus()}
+          placeholder="email입력"
+          returnKeyType="next"
+        />
+        <Input
+          ref={passwordRef}
+          label="Password"
+          value={password}
+          onChangeText={(text) => setPassword(removeWhitespace(text))}
+          onSubmitEditing={() => passwordConfirmRef.current.focus()}
+          placeholder="Password 입력"
+          returnKeyType="done"
+          isPassword
+        />
+        <Input
+          ref={passwordConfirmRef}
+          label="Password Confirm"
+          value={passwordConfirm}
+          onChangeText={(text) => setPasswordConfirm(removeWhitespace(text))}
+          onSubmitEditing={() => _handleSignupButtonPress}
+          placeholder="Password 다시 입력"
+          returnKeyType="done"
+          isPassword
+        />
+        <ErrorText>{errorMessage}</ErrorText>
+        <Button
+          title="회원가입"
+          onPress={_handleSignupButtonPress}
+          disabled={disabled}
+        />
+      </Container>
+    </KeyboardAwareScrollView>
   );
 };
 
